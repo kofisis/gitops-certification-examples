726be7e3d961c17fb885291a8e7bef0bb0220938
Nana Amako <52378242+kofisis@users.noreply.github.com>
2022-01-22 19:56:23 +0100

    updated the script

--- sample/argo-roll-controller.sh
+++ sample/argo-roll-controller.sh
@@ -1,9 +1,11 @@
 #script to install argocd rollout-controller using the argo cli
 
-argocd app create demo \
+argocd app create argo-rollouts-controller \
 --project default \
 --repo https://github.com/codefresh-contrib/gitops-certification-examples \
---path ./canary-app \
+--path ./argo-rollouts-controller \
 --sync-policy auto \
 --dest-namespace default \
 --dest-server https://kubernetes.default.svc
+
+# a new namespace has to be created for the rollout-manager
